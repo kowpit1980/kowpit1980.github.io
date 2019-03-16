093eae4086dacde44c9cd353136cc94ad1336e01
Piotr Kowalewski <kowpit1980@gmail.com>
2019-03-16 14:59:49 +0100

    my first react app

---
+++ precache-manifest.0ac5b1d0c3620dc634807427d5f4e308.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "09f3345edab3fd51f06b",
+    "url": "/static/js/main.7de732f5.chunk.js"
+  },
+  {
+    "revision": "0d009dc82880aa75a7e2",
+    "url": "/static/js/2.2c8f914b.chunk.js"
+  },
+  {
+    "revision": "09f3345edab3fd51f06b",
+    "url": "/static/css/main.3f405c12.chunk.css"
+  },
+  {
+    "revision": "0d009dc82880aa75a7e2",
+    "url": "/static/css/2.c9444a4f.chunk.css"
+  },
+  {
+    "revision": "a5af99dea4eb281b748084d875faf7cc",
+    "url": "/index.html"
+  }
+];
